a6efb50899ebd5c150c5305228ac3be4a1853d27
Clement_Alpha <956163372@qq.com>
2019-04-29 13:22:50 +0800

    笔记更新

--- exams/testTwo.js
+++
@@ -1,4 +0,0 @@
-// 将一个字符串中的空格替换成“%20”
-function replaceSpace(str){
-    return str.replace(/ /g, '%20');
-}
--- exams/testTwolve.js
+++
@@ -1,4 +0,0 @@
-// 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
-function Power(base, exponent){
-    return Math.pow(base, exponent);
-}
